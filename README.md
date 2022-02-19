51d0917b473869903acf47a50ccf7d5f36c0b29a
Jonas Ulrich <136760+undefined@users.noreply.github.com>
2022-02-19 15:14:06 +0000

    First commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# tsnm-ds
-Blank starter kit - for the most adventurous.
---
+++ mdx-layout/src/mdx-layout.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { MDXProvider } from '@mdx-js/react';
+import { CoreLayout } from '@divriots/dockit-react/mdx-layout-core';
+
+import '@divriots/dockit-react/style.css';
+import '~/mdx-layout/src/styleshowcase.css';
+import '~/mdx-layout/src/docs.scss';
+
+import '@kickstartds/design-system/dist/index.css';
+import '@kickstartds/design-system/dist/index.js';
+
+import '~/colors/src/tokens.css';
+
+export const Layout = (props) => (
+  <MDXProvider>
+    <CoreLayout
+      logo={
+        <div className="logo">
+          <img
+            className="logo__image"
+            src="https://i1.sndcdn.com/avatars-000003159261-cat82d-t200x200.jpg"
+          />
+          <span className="logo__text">Personal homepage of TSNM</span>
+        </div>
+      }
+      {...props}
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/water.css@2/out/light.css"
+    />
+  </MDXProvider>
+);
